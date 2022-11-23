a085fb9db1ed764cfb02815519c82dd191990d6b
AtsushiHosaka <hosaka2917@gmail.com>
2022-11-23 16:40:43 +0900

    Initial Commit

---
+++ RGBGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBGame
+//
+//  Created by 保坂篤志 on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
